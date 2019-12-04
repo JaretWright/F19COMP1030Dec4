8be2e3951726ee895e5df5f994ee1592383f658e
JaretWright <jaret.wright@georgiancollege.ca>
2019-12-04 14:15:53 -0500

    Added writing to files

---
+++ src/WriteToFile.java
@@ -0,0 +1,29 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Formatter;
+
+public class WriteToFile {
+    public static void main(String[] args)  {
+        try {
+
+            //to make it so you can append to a file
+            FileWriter fw = new FileWriter("appendFile.txt", true);
+            Formatter outputFile = new Formatter(fw);
+
+
+            outputFile.format("1 week left\n");
+            outputFile.format("Lunch cost $%6.2f\n", 64.73);
+            outputFile.format("Utsav has gifts for everyone\n");
+
+            outputFile.close();
+            fw.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
